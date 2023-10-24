aff855921e8a28cd280932e31c20513bee759944
leeeunjeong <eunjeong990731@gmail.com>
2023-10-24 18:59:31 +0900

    가짜 데이터베이스 만들고 적용

--- app.js
+++ app.js
@@ -19,6 +19,37 @@ const server = http.createServer((req, res) => {
       }
     });
   }
+
+  const fakeDatabase = {
+    images: [],
+  };
+
+  // 데이터베이스 초기화
+  function initializeDatabase() {
+    const initialData = {
+      images: [
+        { id: 1, path: "uploads/image1.jpg", comment: "첫 번째 이미지" },
+        { id: 2, path: "uploads/image2.jpg", comment: "두 번째 이미지" },
+        // 추가 데이터
+      ],
+    };
+
+    fs.writeFileSync("fakeDatabase.json", JSON.stringify(initialData));
+  }
+
+  // 데이터베이스 파일 읽기
+  function readDatabase() {
+    const data = fs.readFileSync("fakeDatabase.json", "utf8");
+    return JSON.parse(data);
+  }
+
+  // 가짜 데이터베이스 초기화 및 데이터 읽어오기
+  initializeDatabase();
+  const database = readDatabase();
+
+  console.log(database);
+
+  // 이제 데이터베이스 변수 `database`를 사용하여 데이터를 읽고 쓸 수 있습니다.
 });
 
 const port = 3000;
